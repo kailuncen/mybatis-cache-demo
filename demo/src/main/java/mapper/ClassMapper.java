ea9beceee2ed6806f0753b59fa56c44a64b06224
cenkailun <cenkailun@meituan.com>
2017-07-01 19:43:47 +0800

    commit

--- demo/src/main/java/mapper/ClassMapper.java
+++
@@ -1,8 +0,0 @@
-package mapper;
-
-import org.apache.ibatis.annotations.Param;
-
-public interface ClassMapper {
-
-    public int updateClassName(@Param("name") String className, @Param("id") int id);
-}
